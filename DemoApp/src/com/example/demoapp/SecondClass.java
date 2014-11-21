d00535085a5f5ab5ae342400b5f33819f63cea3e
Satish Yanamadala <satishgitam@gmail.com>
2014-11-21 15:50:11 +0530

    Second Class

---
+++ DemoApp/src/com/example/demoapp/SecondClass.java
@@ -0,0 +1,14 @@
+package com.example.demoapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondClass extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
